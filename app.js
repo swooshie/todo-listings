e0cf9a7d1a140ed9bc2a4194c6be6cc4595156e0
swooshie <57328174+swooshie@users.noreply.github.com>
2020-03-17 09:28:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var todoController = require('./controllers/todoController.js');
+
+
+var app = express();
+
+//set templat engine 
+app.set('view engine','ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(3000);
+
+console.log('You are listening to port 3000');
